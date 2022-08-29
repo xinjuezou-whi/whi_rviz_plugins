3685ca56528f192363a0187fdff77592098d0ac5
Xinjue Zou <hit_zou@126.com>
2022-08-29 21:13:58 +0800

    added battery body

--- scripts/send_test_msgs.py
+++ scripts/send_test_msgs.py
@@ -13,13 +13,15 @@ rospy.init_node(topic)
 
 br = tf.TransformBroadcaster()
 rate = rospy.Rate(10)
-angle = 0
+roll = 0
+pitch = 0
+yaw = 0
 
 step = 5
 while not rospy.is_shutdown():
 
     battery = WhiBattery()
-    battery.header.frame_id = "/base_link"
+    battery.header.frame_id = "base_link"
     battery.header.stamp = rospy.Time.now()
    
     battery.percent = step;
@@ -27,7 +29,7 @@ while not rospy.is_shutdown():
     publisher.publish(battery)
 
     br.sendTransform((0, 0, 0),
-                     tf.transformations.quaternion_from_euler(0, 0, angle),
+                     tf.transformations.quaternion_from_euler(roll, pitch, yaw),
                      rospy.Time.now(),
                      "base_link",
                      "map")
