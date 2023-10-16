80f1d4f5bd1bd2080b41a761ff40c2f0ae79c0e3
Xinjue Zou <hit_zou@126.com>
2023-10-16 13:26:26 +0800

    fix unit of linear velocity

--- src/display_state.cpp
+++ src/display_state.cpp
@@ -39,7 +39,7 @@ namespace whi_rviz_plugins
         : Display()
         , node_handle_(std::make_unique<ros::NodeHandle>())
     {
-        std::cout << "\nWHI RViz plugin for motion state VERSION 00.02.0" << std::endl;
+        std::cout << "\nWHI RViz plugin for motion state VERSION 00.02.1" << std::endl;
         std::cout << "Copyright @ 2023-2024 Wheel Hub Intelligent Co.,Ltd. All rights reserved\n" << std::endl;
 
         tf_listener_ = std::make_unique<tf2_ros::TransformListener>(buffer_);
