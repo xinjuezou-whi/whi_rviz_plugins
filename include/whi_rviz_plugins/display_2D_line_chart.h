460d96aee18abc0b575e837456dc1255a4469adf
Xinjue Zou <hit_zou@126.com>
2025-08-06 15:13:59 +0800

    migrate 2d line chart from ros1

---
+++ include/whi_rviz_plugins/display_2D_line_chart.h
@@ -0,0 +1,83 @@
+/******************************************************************
+rviz plugin for 2D line chart
+
+Features:
+- 2D line
+- xxx
+
+Written by Xinjue Zou, xinjue.zou.whi@gmail.com
+
+Apache License Version 2.0, check LICENSE for more information.
+All text above must be included in any redistribution.
+
+Changelog:
+2024-07-29: Initial version
+2025-08-06: Migrate from ROS 1
+2025-xx-xx: xxx
+******************************************************************/
+#pragma once
+#include "panel_2D_line_chart.h"
+
+#include <rviz_common/display.hpp>
+#include <rviz_common/panel_dock_widget.hpp>
+
+// forward declaration
+namespace rviz_common
+{
+	namespace properties
+	{
+		class RosTopicProperty;
+		class IntProperty;
+		class FloatProperty;
+		class ColorProperty;
+	}
+}
+
+namespace whi_rviz_plugins
+{
+	// declare a new subclass of rviz::Display
+	// every display which can be listed in the "Displays" panel is a subclass of rviz::Display
+	class Display2DLineChart : public rviz_common::Display 
+	{
+		Q_OBJECT
+	public:
+		// pluginlib::ClassLoader creates instances by calling the default constructor,
+		// so make sure you have one
+		Display2DLineChart();
+		virtual ~Display2DLineChart();
+
+		// overrides of protected virtual functions from Display as much as possible,
+		// when Displays are not enabled, they should not be subscribed to incoming data,
+		// and should not show anything in the 3D view
+		// these functions are where these connections are made and broken
+	protected:
+		virtual void onInitialize();
+
+	private Q_SLOTS:
+		// these Qt slots get connected to signals indicating changes in the user-editable properties
+		void updateDataTopic();
+		void updateMaxDataLength();
+		void updateGridDataSize();
+		void updateGridMajorSize();
+		void updateGridMinorSize();
+		void updateDataColor();
+		void updateMajorColor();
+		void updateMinorColor();
+		void updateCanvasColor();
+
+	private:
+        rviz_common::PanelDockWidget* frame_dock_{ nullptr };
+        LineChart2DPanel* panel_{ nullptr };
+
+		// user-editable property variables
+		rviz_common::properties::RosTopicProperty* data_topic_property_;
+		rviz_common::properties::IntProperty* max_data_length_property_;
+		rviz_common::properties::FloatProperty* data_size_property_;
+		rviz_common::properties::FloatProperty* grid_major_size_property_;
+		rviz_common::properties::FloatProperty* grid_minor_size_property_;
+		rviz_common::properties::ColorProperty* data_color_property_;
+		rviz_common::properties::ColorProperty* grid_major_color_property_;
+		rviz_common::properties::ColorProperty* grid_minor_color_property_;
+		rviz_common::properties::ColorProperty* canvas_color_property_;
+	};
+} // end namespace whi_rviz_plugins
---
+++ src/display_2D_line_chart.cpp
@@ -0,0 +1,128 @@
+/******************************************************************
+rviz plugin for 2D line chart
+
+Features:
+- 2D line
+- xxx
+
+Written by Xinjue Zou, xinjue.zou.whi@gmail.com
+
+Apache License Version 2.0, check LICENSE for more information.
+All text above must be included in any redistribution.
+
+******************************************************************/
+#include "whi_rviz_plugins/display_2D_line_chart.h"
+
+#include <rviz_common/properties/ros_topic_property.hpp>
+#include <rviz_common/properties/int_property.hpp>
+#include <rviz_common/properties/float_property.hpp>
+#include <rviz_common/properties/color_property.hpp>
+#include <rviz_common/window_manager_interface.hpp>
+#include <rviz_common/display_context.hpp>
+
+namespace whi_rviz_plugins
+{
+    Display2DLineChart::Display2DLineChart()
+        : Display()
+    {
+        std::cout << "\nWHI RViz plugin for 2D line chart VERSION 02.02.1" << std::endl;
+        std::cout << "Copyright @ 2024-2026 Wheel Hub Intelligent Co.,Ltd. All rights reserved\n" << std::endl;
+
+        data_topic_property_ = new rviz_common::properties::RosTopicProperty("Data topic", "line_data_2D",
+            "whi_interfaces/WhiLineChart2D", "Topic of 2D data",
+            this, SLOT(updateDataTopic()));
+        max_data_length_property_ = new rviz_common::properties::IntProperty("Max data length", 100,
+            "Maxium data length that recoreded for plotting", this, SLOT(updateMaxDataLength()));
+        max_data_length_property_->setMin(1);
+        data_size_property_ = new rviz_common::properties::FloatProperty("Data size", 2.0, "Data grid size",
+            this, SLOT(updateGridDataSize()));
+        data_color_property_ = new rviz_common::properties::ColorProperty("Data grid color", Qt::yellow, "Data grid color.",
+            this, SLOT(updateDataColor()));
+        grid_major_size_property_ = new rviz_common::properties::FloatProperty("Major grid size", 0.5, "Major grid size",
+            this, SLOT(updateGridMajorSize()));
+        grid_major_color_property_ = new rviz_common::properties::ColorProperty("Major grid color", Qt::cyan,
+            "Major grid color.", this, SLOT(updateMajorColor()));
+        grid_minor_size_property_ = new rviz_common::properties::FloatProperty("Minor grid size", 0.2, "Minor grid size",
+            this, SLOT(updateGridMinorSize()));
+        grid_minor_color_property_ = new rviz_common::properties::ColorProperty("Minor grid color", Qt::gray,
+            "Minor grid color.", this, SLOT(updateMinorColor()));
+        canvas_color_property_ = new rviz_common::properties::ColorProperty("Canvas color", QColor(38, 64, 115),
+            "Canvas grid color.", this, SLOT(updateCanvasColor()));
+    }
+
+    Display2DLineChart::~Display2DLineChart()
+    {
+        delete frame_dock_;
+    }
+    
+    void Display2DLineChart::onInitialize()
+    {
+        Display::onInitialize();
+
+        panel_ = new LineChart2DPanel();
+        rviz_common::WindowManagerInterface* windowContext = context_->getWindowManager();
+        if (windowContext)
+        {
+            frame_dock_ = windowContext->addPane("2DLineChart", panel_); // getName() return "" ???
+            frame_dock_->setIcon(getIcon()); // set the image name as same as the name of plugin
+        }
+
+        updateDataTopic();
+        updateMaxDataLength();
+		updateGridDataSize();
+		updateGridMajorSize();
+		updateGridMinorSize();
+		updateDataColor();
+		updateMajorColor();
+		updateMinorColor();
+		updateCanvasColor();
+    }
+
+    void Display2DLineChart::updateDataTopic()
+    {
+        panel_->setDataTopic(data_topic_property_->getTopicStd());
+    }
+
+    void Display2DLineChart::updateMaxDataLength()
+    {
+        panel_->setMaxDataLength(max_data_length_property_->getInt());
+    }
+
+    void Display2DLineChart::updateGridDataSize()
+    {
+        panel_->setGridDataSize(data_size_property_->getFloat());
+    }
+
+    void Display2DLineChart::updateGridMajorSize()
+    {
+        panel_->setGridMajorSize(grid_major_size_property_->getFloat());
+    }
+
+    void Display2DLineChart::updateGridMinorSize()
+    {
+        panel_->setGridMinorSize(grid_minor_size_property_->getFloat());
+    }
+
+    void Display2DLineChart::updateDataColor()
+    {
+        panel_->setGridDataColor(data_color_property_->getColor());
+    }
+
+    void Display2DLineChart::updateMajorColor()
+    {
+        panel_->setGridMajorColor(grid_major_color_property_->getColor());
+    }
+
+    void Display2DLineChart::updateMinorColor()
+    {
+        panel_->setGridMinorColor(grid_minor_color_property_->getColor());
+    }
+
+    void Display2DLineChart::updateCanvasColor()
+    {
+        panel_->setGridCanvasColor(canvas_color_property_->getColor());
+    }
+} // end namespace whi_rviz_plugins
+
+#include <pluginlib/class_list_macros.h>
+PLUGINLIB_EXPORT_CLASS(whi_rviz_plugins::Display2DLineChart, rviz_common::Display)
